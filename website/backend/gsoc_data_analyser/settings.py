0df253e62e1b9f90756814a1b3e830da82e4932f
dojutsu-user <vaibhgupt199@gmail.com>
2018-10-25 13:20:56 +0530

    Install and setting up django-cors-headers

--- website/backend/gsoc_data_analyser/settings.py
+++ website/backend/gsoc_data_analyser/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+  'localhost:3000/'
+)
