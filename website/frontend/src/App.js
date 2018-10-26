2628e8cb3479eb1bdc93ea3b3278639dffb74a37
dojutsu-user <vaibhgupt199@gmail.com>
2018-10-26 14:47:18 +0530

    Add a navbar

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import AppBar from "./components/AppBar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <AppBar />
       </div>
     );
   }
---
+++ website/frontend/src/components/AppBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
+
+const appbar = props => {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            GSoC Data Analyzer
+          </Typography>
+          <Button color="inherit">GSoC Overview</Button>
+          <Button color="inherit">About</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+appbar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(appbar);
